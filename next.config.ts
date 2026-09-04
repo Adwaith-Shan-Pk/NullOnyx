8270d365a23750287723f8dbe93b5385c6066ac8
Adwaith Shan Pk <pkadwaithshan@gmail.com>
2026-09-05 02:25:45 +0530

    Changed PWA package to newer serwist/next

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
+import withSerwistInit from "@serwist/next";
 
-const withPWA = require("@ducanh2912/next-pwa").default({
-  dest: "public",
+const withSerwist = withSerwistInit({
+  swSrc: "src/app/sw.ts",
+  swDest: "public/sw.js",
   disable: process.env.NODE_ENV === "development",
-  register: true,
-  skipWaiting: true,
 });
 
 const nextConfig: NextConfig = {
@@ -12,4 +12,4 @@ const nextConfig: NextConfig = {
   turbopack: {},
 };
 
-export default withPWA(nextConfig);
+export default withSerwist(nextConfig);
